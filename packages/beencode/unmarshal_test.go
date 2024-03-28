9a996c32f22e32ab565421ac8d72d096d93ffb86
kyryl.perepelytsia <kyryl.perepelytsia@globalpay.com>
2024-03-28 18:20:21 +0100

    fix order expect/actual

--- packages/beencode/unmarshal_test.go
+++ packages/beencode/unmarshal_test.go
@@ -9,23 +9,23 @@ import (
 func TestUnmarshalString(t *testing.T) {
 	r := strings.NewReader("3:foo")
 	res, _ := Unmarshal(r)
-	assert.Equal(t, res, "foo")
+	assert.Equal(t, "foo", res)
 }
 
 func TestUnmarshalInt(t *testing.T) {
 	r := strings.NewReader("i120e")
 	res, _ := Unmarshal(r)
-	assert.Equal(t, res, 120)
+	assert.Equal(t, 120, res)
 }
 
 func TestUnmarshalList(t *testing.T) {
 	r := strings.NewReader("l3:foo3:bare")
 	res, _ := Unmarshal(r)
-	assert.Equal(t, res, []interface{}{"foo", "bar"})
+	assert.Equal(t, []interface{}{"foo", "bar"}, res)
 }
 
 func TestUnmarshalDictionary(t *testing.T) {
 	r := strings.NewReader("d7:astring3:str4:binti120e5:clistl4:listee")
 	res, _ := Unmarshal(r)
-	assert.Equal(t, res, map[string]interface{}{"astring": "str", "bint": 120, "clist": []interface{}{"list"}})
+	assert.Equal(t, map[string]interface{}{"astring": "str", "bint": 120, "clist": []interface{}{"list"}}, res)
 }
