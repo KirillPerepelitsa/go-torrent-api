a2d75234d35057e5cd7d008e09298d1dea8c4926
kyryl.perepelytsia <kyryl.perepelytsia@globalpay.com>
2024-04-02 13:59:41 +0200

    handle case when string has big length and not fits in a buffer

--- packages/beencode/unmarshal.go
+++ packages/beencode/unmarshal.go
@@ -2,10 +2,10 @@ package beencode
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"strconv"
-	"strings"
 )
 
 func Unmarshal(reader io.Reader) (interface{}, error) {
@@ -18,11 +18,17 @@ func Unmarshal(reader io.Reader) (interface{}, error) {
 
 func decode(r *bufio.Reader) (interface{}, error) {
 	char, err := r.ReadByte()
+	//s := r.Size()
+	//buf := make([]byte, s)
+	//_, err = r.Read(buf)
+	//println(err)
+	//println(string(buf))
+	//return nil, errors.New("FOO")
 	if err != nil {
 		return nil, err
 	}
-	switch char {
-	case 'l':
+	switch {
+	case char == 'l':
 		var l []interface{}
 		for {
 			b, e := r.ReadByte()
@@ -42,13 +48,13 @@ func decode(r *bufio.Reader) (interface{}, error) {
 			}
 			l = append(l, item)
 		}
-	case 'i':
-		i, err := r.ReadBytes('e')
+	case char == 'i':
+		i, err := readUntil(r, 'e')
 		if err != nil {
 			return nil, err
 		}
-		return strconv.Atoi(strings.Trim(string(i), "e"))
-	case 'd':
+		return strconv.Atoi(string(i))
+	case char == 'd':
 		d := make(map[string]interface{})
 		for {
 			c, err := r.ReadByte()
@@ -73,26 +79,73 @@ func decode(r *bufio.Reader) (interface{}, error) {
 			v, err := decode(r)
 			d[key] = v
 		}
-	default:
+	case char >= '0' && char <= '9':
 		err := r.UnreadByte()
 		if err != nil {
 			return nil, err
 		}
+		var strLen int64
+		if s, err := readUntil(r, ':'); err == nil {
+			strLen, err = strconv.ParseInt(string(s), 10, 64)
+		}
+		if err != nil {
+			return nil, err
+		}
 
-		num, _, err := strLen(r)
-		if err != nil {
-			return nil, err
+		// Can we peek that much data out of r?
+		if peekBuf, peekErr := r.Peek(int(strLen)); peekErr == nil {
+			_, err = r.Discard(int(strLen))
+			if err != nil {
+				return nil, err
+			}
+			return string(peekBuf), nil
 		}
-		if err != nil {
-			return nil, err
-		}
-		buf := make([]byte, num)
-		_, err = r.Read(buf)
+
+		buf := make([]byte, strLen)
+		_, err = readFull(r, buf)
 		if err != nil {
 			return nil, err
 		}
 		return string(buf), nil
+
+	default:
+		return r, fmt.Errorf("wrong starting character %s", string(char))
 	}
+}
+
+func readFull(r *bufio.Reader, buf []byte) (n int, err error) {
+	return readAtLeast(r, buf, len(buf))
+}
+
+func readAtLeast(r *bufio.Reader, buf []byte, min int) (n int, err error) {
+	if len(buf) < min {
+		return 0, io.ErrShortBuffer
+	}
+	for n < min && err == nil {
+		var nn int
+		nn, err = r.Read(buf[n:])
+		n += nn
+	}
+	if n >= min {
+		err = nil
+	} else if n > 0 && err == io.EOF {
+		err = io.ErrUnexpectedEOF
+	}
+	return
+}
+
+func readUntil(r *bufio.Reader, delim byte) (data []byte, err error) {
+	data, err = r.ReadSlice(delim)
+	if err != nil {
+		return nil, err
+	}
+	lenData := len(data)
+	if lenData > 0 {
+		data = data[:lenData-1]
+	} else {
+		err = errors.New("bad r.ReadSlice() length")
+	}
+	return
 }
 
 func strLen(r *bufio.Reader) (num int, l int, err error) {
